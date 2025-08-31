91755695249faad5ffa39f402d4a15de69329711
maulanaahmad127 <maulahmaddd40@gmail.com>
2025-09-01 06:22:35 +0700

    add new logic

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -9,6 +9,7 @@ import calc
 argnumbers = len(sys.argv) - 1
 
 if argnumbers == 2 :
+    print("my name is lana")
     print("")
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
